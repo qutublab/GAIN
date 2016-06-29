930d799b5bbc555ec280ccf8a44a59ff8ff6040f
quentinli8 <hl43@rice.edu>
2016-06-29 10:01:38 -0500

    Update sliders - 6/29
    Opening a parameter file will update the sliders as well as the edit boxes.

--- updateControlPanel.m
+++ updateControlPanel.m
@@ -1,6 +1,6 @@
 %update the parameter values when the user opens a parameter file
 
-function [editBoxes_new] = updateControlPanel(editBoxes, oneParameterArr, h)
+function [editBoxes_new, hSliders_new] = updateControlPanel(editBoxes, oneParameterArr, h, hSliders)
 % leftMargin=10;
 % bottomMargin=10;
 % pushButtonWidth=130;
@@ -21,9 +21,12 @@ function [editBoxes_new] = updateControlPanel(editBoxes, oneParameterArr, h)
 for i=numel(oneParameterArr):-1:2
     %     editBoxes_new(i)=createEditBox(h,i,oneParameterArr(i),[leftMargin bottom editBoxWidth editBoxHeight],[leftMargin+editBoxWidth+horizontalSpace bottom textBoxWidth textBoxHeight]);
     %     bottom=bottom+(editBoxHeight+verticalSpace);
-    set(editBoxes(i), 'string', oneParameterArr(i).value)
+    set(editBoxes(i), 'string', oneParameterArr(i).value) %update editbox values
+    textValue(i-1) = str2num(oneParameterArr(i).value);
+    set(hSliders(i-1),'Value', textValue(i-1)) %update slider positions
 end
 editBoxes_new = editBoxes;
+hSliders_new = hSliders;
 
 end
 
