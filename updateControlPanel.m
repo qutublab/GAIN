a26a196083869edc205592d5d3071fa6de70d33f
qutublab <qutublab@gmail.com>
2016-12-07 15:39:39 -0600

    Update GAIN_part 2 12/7/16

--- updateControlPanel.m
+++ updateControlPanel.m
@@ -1,17 +1,24 @@
-%update the parameter values when the user opens a parameter file
+function [Edit_Box] = updateControlPanel(oneParameterArr, h)
+leftMargin=10;
+bottomMargin=10;
+pushButtonWidth=130;
+pushButtonHeight=30;
+horizontalSpace=20;
+fileNameBox = 330;
+editBoxHeight=20;
+editBoxWidth=60;
+verticalSpace=20;  %spacing between edit boxes
+textBoxHeight=20;  %spacing between text boxes
+textBoxWidth=250;
+totalwidth = leftMargin + editBoxWidth + horizontalSpace + textBoxWidth;
 
-function [editBoxes_new, hSliders_new] = updateControlPanel(editBoxes, oneParameterArr, h, hSliders)
+bottom=bottomMargin+pushButtonHeight+verticalSpace;
+bottom=bottom+textBoxHeight+15;
+bottom=bottom+pushButtonHeight+verticalSpace;
 
 for i=numel(oneParameterArr):-1:2
-    %     editBoxes_new(i)=createEditBox(h,i,oneParameterArr(i),[leftMargin bottom editBoxWidth editBoxHeight],[leftMargin+editBoxWidth+horizontalSpace bottom textBoxWidth textBoxHeight]);
-    %     bottom=bottom+(editBoxHeight+verticalSpace);
-    set(editBoxes(i), 'string', oneParameterArr(i).value) %update editbox values
-    textValue(i-1) = str2num(oneParameterArr(i).value);
-    set(hSliders(i-1),'Value', textValue(i-1)) %update slider positions
+    Edit_Box(i)=createEditBox(h,i,oneParameterArr(i),[leftMargin bottom editBoxWidth editBoxHeight],[leftMargin+editBoxWidth+horizontalSpace bottom textBoxWidth textBoxHeight]);
+    bottom=bottom+(editBoxHeight+verticalSpace);
 end
-
-editBoxes_new = editBoxes;
-hSliders_new = hSliders;
-
 end
 
